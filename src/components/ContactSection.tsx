0ff50b7156514a0c235a9277fc06438d67c82888
Hesbon Angwenyi <hesbonmanyinsa96@gmail.com>
2025-11-21 00:33:07 +0300

    updated

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -61,7 +61,7 @@ const ContactSection: React.FC = () => {
       id="contact"
       className="relative py-20 md:py-24 min-h-[80vh] overflow-hidden"
       style={{
-        backgroundImage: "url('https://i.pinimg.com/736x/97/a2/18/97a218ca8e7c4636f9d1d3964d3d2b0d.jpg')", // <-- Local image
+        backgroundImage: "url('https://i.pinimg.com/736x/4a/56/e8/4a56e8884154f8f138742f360ac5f9b8.jpg')", // <-- Local image
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
