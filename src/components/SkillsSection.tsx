0f59468b1c16b02014ccf8e67119b6568439b658
Hesbon Angwenyi <hesbonmanyinsa96@gmail.com>
2025-12-23 06:57:06 +0300

    updated

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -42,7 +42,7 @@ const Stats = () => {
                 { name: "Docker", level: 90 },
                 { name: "Kubernetes", level: 75 },
                 { name: "Ansible", level: 80 },
-                { name: "Terraform", level: 78 },
+                { name: "Terraform", 
                 { name: "Git & GitHub", level: 92 },
                 { name: "CI/CD Pipelines", level: 85 },
                 { name: "AWS", level: 60 },
