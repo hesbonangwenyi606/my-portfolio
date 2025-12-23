d0668f0c16d93418121ba242fac325493ec3cf5f
Hesbon Angwenyi <hesbonmanyinsa96@gmail.com>
2025-12-23 06:58:40 +0300

    updated

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -41,7 +41,7 @@ const Stats = () => {
               skills={[
                 { name: "Docker", level: 90 },
                 { name: "Kubernetes", level: 75 },
-                { name: "Ansib
+                { name: 
                 { name: "Git & GitHub", level: 92 },
                 { name: "CI/CD Pipelines", level: 85 },
                 { name: "AWS", level: 60 },
