30a1e28c53e564d5413526737ce4cd338f362b45
Hesbon Angwenyi <hesbonmanyinsa96@gmail.com>
2026-01-04 00:22:40 +0300

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -87,7 +87,7 @@ const Footer: React.FC = () => {
             <h3 className="text-lg font-semibold mb-3">Services</h3>
             <ul className="space-y-1.5 text-gray-300">
               <li>Full-Stack Development</li>
-              <li>Basic Cloud Deployment</li>
+              <ul>Basic Cloud Deployment</ul>
               <li>Web Development</li>
               <li>Backend Development (Entry-Level)</li>
             </ul>
