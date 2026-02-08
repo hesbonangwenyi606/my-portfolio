36b12f2802087a3eeb83243466a321ec668dfd77
hesbonangwenyi606 <hesbonmanyinsa96@gmail.com>
2026-02-08 14:57:40 +0300

    update

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -102,7 +102,7 @@ const education: EducationItem[] = [
   },
   {
     school: "KCA UNIVERSITY",
-    qualification: "Bachelor’s degree in Mathematics & Computer Science",
+    qualification: "Bachelor degree in Mathematics & Computer Science",
     period: "2020 - 2023",
     icon: <FaGraduationCap className="text-gray-900" />,
     description:
